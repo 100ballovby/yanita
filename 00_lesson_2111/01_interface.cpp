c57fcf71808b13cfc6fef114f8b264805d51c8fa
demidraksin <greatraksin@icloud.com>
2022-11-28 20:15:57 +0300

    Adding control

---
+++ 00_lesson_2111/01_interface.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int randint(int min, int max);
+
+
+
+int randint(int min, int max) {
+    return rand() % (max - min) + min;
+}
+
+int check_int() {
+    while (true) {
+        cout << "Введите 0 для выхода из программы или 1, чтобы запустить.";
+        int a;
+        cin >> a;
+
+        if (cin.fail()) {  // если извлечение не удалось
+            cin.clear();  // возвращаем cin в нормальный режим
+            cin.ignore(32767, '\n');  // удаляем предыдущий ввод и символ новой строки из буфера
+        } else {
+            if (a == 1 || a == 0) {
+                return a;
+            }
+        }
+    }
+}
+
+
+int это_main() {
+    cout << "Суть задания: " << endl;
+    cout << "Выполнила ... " << endl;
+    bool repeat;
+    repeat = check_int();
+
+    while (repeat) {
+        // тут вызываем войды
+        repeat = check_int();
+    }
+}
