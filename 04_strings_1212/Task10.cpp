30ef56ef1b97022ae19e0d0871f3fd0b3a73c2db
demidraksin <greatraksin@icloud.com>
2022-12-12 18:48:05 +0300

    Adding dynamic

---
+++ 04_strings_1212/Task10.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    long long n, n1, n2, N1, N2;
+    long long razmer = 100, k1 = 1, k2 = 1,  d = 0;
+    cout << "Розетки: ";
+    cin >> n;
+    if (n % 2 == 0)	{
+        n1 = n / 2;
+        n2 = n - 1;
+    }
+    else{
+        n1 = (n - 1) / 2;
+        n2 = n;
+    }
+    auto *arr1 = new long long[razmer];
+    auto *arr2 = new long long[razmer];
+    auto *arr3 = new long long[razmer];
+    N1 = n1;
+    N2 = n2;
+    while (N1 /= 10) {
+        k1++;
+    };
+    while (N2 /= 10) {
+        k2++;
+    }
+    for (int i = razmer - 1; i >= razmer - 1 - k1; i--){
+        arr1[i] = n1 % 10;
+        n1 /= 10;
+    }
+    cout << endl;
+    for (long int i = razmer - 1; i >= razmer - 1 - k2; i--){
+        arr2[i] = n2 % 10;
+        n2 /= 10;
+    }
+    arr3[razmer - 1] = 1;
+    for (long int i = razmer - 1; i >= razmer - 1 - k1; i--){
+        for (long int j = razmer - 1; j >= razmer - 1 - k2; j--){
+            arr3[j - d] += arr1[i] * arr2[j];
+            arr3[j - d - 1] += arr3[j - d] / 10;
+            arr3[j - d] %= 10;
+        }
+        d++;
+    }
+    arr3[0] = -1;
+    arr3[1] = -1;
+    for (long int i = 2; i < razmer; i++){
+        if (arr3[i] == 0 && arr3[i - 1] == -1) {
+            arr3[i] = -1;
+        }
+
+    }
+    for (long int i = 0; i < razmer; i++){
+        if (arr3[i] != -1) {
+            cout << arr3[i];
+        }
+    }
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,86 @@
 #include <iostream>
 using namespace std;
 
+void char_type() {
+    char a;
+    cout << "Insert keyboard symbol: ";
+    cin >> a;
+    cout << "Symbol is: " << a << endl;
+    cout << "Symbol \nnumber \tis: " << (int)a << endl;
+}
 
-int main() {
+void cin_char() {
+    char arr[5] = {'h', 'e', 'l', 'l', 'o'};
+    char arr1[10];
+    cout << arr << endl;
+    cout << arr1 << endl;
+}
+
+void getchar_method() {
+    int symbol, i = 0;
+    char arr[20];
+    cout << "Insert characters, press Enter to stop" << endl;
+
+    do {
+        symbol = getchar();
+        arr[i] = symbol;
+        i++;
+    } while (symbol != '\n');
+
+    cout << arr << endl;
+}
+
+void lab6_task1() {
+    char st[80];
+    int symbol, i = 0;
+    cout << "Enter string, cancel with Enter: ";
+    do {
+        symbol = getchar();
+        st[i] = symbol;
+        i++;
+    } while (symbol != '\n');
+
+    int count_one = 0;
+    int countn = 0;
+    int counterror = 0;
+    for (int i = 0; i < 80; i++) {
+        if (st[i] == '1') {
+            count_one++;
+        } else if (st[i] == '0') {
+            if (count_one % 2 && count_one > 1) {  // если одиночные единички нужны, уберите && и до конца
+                countn += count_one;
+            }
+            count_one = 0;
+        } else {
+            counterror++;
+        }
+    }
+
+
+    if (count_one % 2) {
+        countn += count_one;
+    }
+    if (counterror) {
+        cout << "There is restricted symbols in string!" << endl;
+    }
+    cout << countn << endl;
+}
+
+void lab6_task2() {
+    char str[80] = "subs, isub, csub.";
+    char s1[80];
+    int j = 0;
+    for (int i = 0; str[i]; i++) {
+        s1[i + j] = str[i];
+        if (str[i] == ',') {
+            s1[i + j + 1] = '!';
+            j++;
+        }
+    }
+    cout << s1 << endl;
+}
+
+
+int main(){
     return 0;
 }
